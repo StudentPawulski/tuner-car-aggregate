0a770c3748a5e851a1387cc32647c0805bd4739a
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-10-05 13:34:41 -0500

    First run on full scrape

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,59 +7,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'httparty'
 require 'nokogiri'
-require 'json'
-require 'pry'
-require 'csv'
 require 'open-uri'
 require 'restclient'
 require 'pp'
 
-province_codes = %w[b-british-columbia b-alberta b-saskatchewan b-manitoba b-ontario b-quebec]
-CARS_HASH = {
+province_codes = { 'b-british-columbia' => 'k0l9007',
+                   'b-alberta' => 'k0l9003',
+                   'b-saskatchewan' => 'k0l9009',
+                   'b-manitoba' => 'k0l9006',
+                   'b-ontario' => 'k0l9004' }
+
+cars_hash = {
   mazda: { model: %w[mx5 rx7] },
   nissan: { model: %w[silvia 240sx 180sx skyline] },
   toyoa: { model: %w[ae86 supra] },
   mitsubishi: { model: %w[evo 3000gt] },
   honda: { model: %w[civic-type-r civic-sir] }
-}.freeze
+}
 
-KIJIJI = 'https://www.kijiji.ca/'
+# For testing
+mazda_hash = {
+  mazda: { model: %w[mx5 rx7] }
+}
 
-province_codes.each do |province|
-  CARS_HASH.each do |make, models|
+# For testing
+manitoba_codes = {
+  'b-manitoba' => 'k0l9006'
+}
+
+KIJIJI = 'https://www.kijiji.ca'
+
+listing_counter = 0
+
+manitoba_codes.each do |province, pcode|
+  mazda_hash.each do |make, models|
     models[:model].each do |model|
-      page = Nokogiri::HTML(RestClient.get("https://www.kijiji.ca/#{province}/#{make}-#{model}/k0l9006?dc=true"))
+      page = Nokogiri::HTML(RestClient.get("#{KIJIJI}/#{province}/#{make}-#{model}/#{pcode}?dc=true"))
 
       listings = page.css('div.clearfix')
 
-      links = []
+      # links = []
 
       listings.each do |listing|
         puts listing.css('div.title').css('a').text.strip
-        links.append listing.css('div.title a').attr('href')
+        puts "Link to car: #{KIJIJI}/#{listing.css('div.title a').attr('href')}"
         puts listing.css('div.price').text.strip
         puts listing.css('div.description').text.strip
         puts listing.css('div.location').text.strip
         puts listing.css('div.image img').attr('src')
         puts ''
+        listing_counter += 1
       end
     end
   end
 end
 
-# links.each do |car_link|
-#  page = Nokogiri::HTML(RestClient.get("#{KIJIJI}#{car_link}"))
-#  puts page
-# end
-
-# page = HTTParty.get('https://www.kijiji.ca/b-manitoba/miata/k0l9006?dc=true')
-# parse_page = Nokogiri::HTML(page)
-
-# File.write('./modelhtml/miata.html', parse_page)
-
-# miata_array = []
-
-# Pry.start(binding)
-# parse_page.css('clearfix')
+puts listing_counter
+puts listing_counter
+puts listing_counter
