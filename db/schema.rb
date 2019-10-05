d1ff771796d9d0a77df64e5cca915f2b9e15096a
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-10-05 14:06:29 -0500

    Created Models along with their relations

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,31 +12,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_190446) do
-
-  create_table "listings", force: :cascade do |t|
-    t.string "title"
-    t.string "make"
-    t.string "model"
-    t.string "url"
-    t.float "price"
-    t.text "description"
-    t.string "location"
-    t.string "image_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_191_005_190_446) do
+  create_table 'listings', force: :cascade do |t|
+    t.string 'title'
+    t.string 'make'
+    t.string 'model'
+    t.string 'url'
+    t.float 'price'
+    t.text 'description'
+    t.string 'location'
+    t.string 'image_url'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "makes", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'makes', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'models', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
