31d1c61b8c9f22c9fc65e5026b7f76ab8b345750
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-10-07 13:54:20 -0500

    Connected index => model => make => listing => Kijiji

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 class ModelsController < ApplicationController
   def index
+    @makes = Make.all
+    @count = Model.count
+    @models = Model.all
+  end
+
+  def show
+    # @make = Make.includes(:models).find(params[:id])
+    @model = Model.includes(:make).find(params[:id])
+    @listings = Listing.all
+    # @modellisting = Model.includes(:listing).find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@
 Rails.application.routes.draw do
   get 'provinces/index'
   get 'models/index'
+  get 'models/show'
   get 'makes/index'
   get 'listings/index'
   get 'listings/show'
 
+  resources :makes, only: %i[index show]
   resources :listings, only: %i[index show]
+  resources :models, only: %i[index show]
 
   root to: 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
